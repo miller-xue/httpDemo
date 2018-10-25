52c25ac7bfaa0c6c4bf5e3fdd02ca20f57aa1d3c
miller_xue <1021362065@qq.com>
2018-10-25 21:44:29 +0800

    数据协商

---
+++ accept/server.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const fs = require('fs')
+const zlib = require('zlib')
+
+http.createServer(function (request, response) {
+    console.log('request come', request.url)
+    // 1.读取html
+    const html = fs.readFileSync('test.html')
+    response.writeHead(200,{
+        'Content-type': 'text/html', // 返回类型
+        'X-Content-Type-Options': 'nosniff',  // 不主动预测返回内容 把文本当脚本执行
+        'Content-Encoding': 'gzip' // 告诉客户端解压方式 加快网络开销
+    })
+    // 2.返回给前台  用zlib进行压缩
+    response.end(zlib.gzipSync(html))
+}).listen(8888)
+
+console.log('server listening on 8888')
--- cors/server.js
+++ cors/server.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 
 http.createServer(function (request, response) {
     console.log('request come', request.url)
-
+    // 1.读取html
     const html = fs.readFileSync('test.html','utf8')
     response.writeHead(200,{
-        'Content-type': 'text/html'
+        'Content-type': 'text/html' // 返回类型
     })
-
+    // 2.返回给前台
     response.end(html)
 }).listen(8888)
 
